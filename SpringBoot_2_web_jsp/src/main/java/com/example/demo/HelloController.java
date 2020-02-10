8df80bce4b3a4378b0b97fbfffa51e24138aa5aa
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-11 00:27:05 +0530

    Add files via upload

---
+++ SpringBoot_2_web_jsp/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController 
+{
+	@RequestMapping("hello")
+	public String hello()
+	{
+		System.out.println("Hello CLass");
+		return "NewFile.jsp";
+	}
+}
