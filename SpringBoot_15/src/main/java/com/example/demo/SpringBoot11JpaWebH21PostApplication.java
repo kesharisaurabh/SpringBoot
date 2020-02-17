527f6b56f75da9bea2f668eca24aee86ddf3f7dd
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-18 00:34:03 +0530

    Add files via upload

---
+++ SpringBoot_15/src/main/java/com/example/demo/SpringBoot11JpaWebH21PostApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot11JpaWebH21PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot11JpaWebH21PostApplication.class, args);
+	}
+
+}
