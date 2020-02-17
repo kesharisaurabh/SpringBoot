823c27e84973e8cd5adb742b436ed719c84db1bd
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-18 00:23:44 +0530

    Add files via upload

---
+++ SpringBoot_13_jpa_web_h2-2/src/main/java/com/example/demo/dao/studentdao.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.student;
+
+public interface studentdao extends JpaRepository<student, Integer>
+{
+			List<student> findbyTec(String tec);
+			List<student> findbyaidGreaterThan(int aid);
+}
