c456e3934cb30510ba055e253ad94a3a65a1578d
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-18 00:49:54 +0530

    Add files via upload

---
+++ SpringBoot_17_Xml_OutputOnly/src/main/java/com/example/demo/model/student.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class student
+{
+	@Id
+	private int aid;
+	private String aname;
+	private String atec;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getAtec() {
+		return atec;
+	}
+	public void setAtec(String atec) {
+		this.atec = atec;
+	}
+	@Override
+	public String toString() {
+		return "student [aid=" + aid + ", aname=" + aname + ", atec=" + atec + "]";
+	}
+	
+	
+}
