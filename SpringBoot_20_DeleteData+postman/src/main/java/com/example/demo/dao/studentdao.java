383f068085d25a456369a6f9e458fe8733c084ea
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-19 00:05:47 +0530

    Add files via upload

---
+++ SpringBoot_20_DeleteData+postman/src/main/java/com/example/demo/dao/studentdao.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.student;
+
+public interface studentdao extends JpaRepository<student, Integer>
+{
+			 
+}
